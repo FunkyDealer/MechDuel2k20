ebd0cfd441054f9d4f7ce4eef227feeff109b594
Cteps <63469310+Cteps@users.noreply.github.com>
2021-01-03 18:45:03 +0000

    Hud

---
+++ Mech Duel 2k20/MechDuel/Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField]
+    Entity[] Players;
+    [SerializeField]
+    Text[] texts;
+    [SerializeField]
+    Text[] names;
+    [SerializeField]
+    GameObject Canvas;
+    bool scoreBoard;
+    private void Start()
+    {
+        Players = FindObjectsOfType<Entity>();
+    }
+    void Update()
+    {
+        int c = 0;
+        foreach(var v in Players) 
+        {
+            names[c].text = v.nickName;
+            texts[c].text =  Convert.ToString(v.score);
+            c++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            scoreBoard = !scoreBoard;
+        }
+        Canvas.SetActive(scoreBoard);
+
+    }
+}
